523489d876db186119d12e90e9a609678256d68a
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-08-07 15:38:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Handeling GET requests
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// Handeling POST requests
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done!');
 });
 
 const port = 3000;
