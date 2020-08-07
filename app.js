9830ac2740174d26cdb6457fc59d2c8694d87e2c
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-08-07 16:38:40 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -20,10 +20,44 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 // Handeling POST requests
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done!');
+  const newId = tours[tours.length - 1].id - 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
