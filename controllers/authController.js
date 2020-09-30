d73f5593e4dba93e08428fbff6906537d4bb7f64
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-09-30 16:44:52 +0530

    Implemented Login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -112,6 +112,32 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1. Verify the token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 2. Check if the user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3. Check if the user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    return next();
+  }
+  next();
+});
+
 // Restrict certain actions
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
