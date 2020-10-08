e4e5938a8cc29f492c4ac6dc8d6cdfee4db44803
aidenfrostbite@gmail.com <aidenfrostbite@gmail.com>
2020-10-08 16:22:45 +0530

    Prepared app for deployment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,7 +36,6 @@ const sendErrDev = (err, req, res) => {
   }
 
   // Rendered website
-  console.error('ERROR!', err);
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: err.message,
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,10 +9,7 @@ const stripe = Stripe(
 export const bookTour = async (tourID) => {
   try {
     // Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourID}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourID}`);
 
     // Create checkout form or charge the CC
     await stripe.redirectToCheckout({
