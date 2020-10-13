a1d3498ccb32a0315b4439c65655466673b95866
aidenfrostbite@gmail.com <aidenfrostbite@gmail.com>
2020-10-13 20:52:12 +0530

    My reviews completed

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ import { signup } from './signup';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
 import { showAlert } from './alerts';
-import { deleteReview } from './updateReview';
+import { deleteReview, updateReview } from './updateReview';
 
 // DOM Elements
 const mapBox = document.getElementById('map');
@@ -88,21 +88,26 @@ if (bookBtn) {
   });
 }
 
+// Update reviews
 if (myReview) {
   myReview.addEventListener('click', (e) => {
     if (e.target !== e.currentTarget) {
       if (e.target.id === 'delete-review') {
         const id = e.target.parentNode.dataset.id;
         confirmAction(id, 'delete');
+      } else if (e.target.id === 'update-review') {
+        const id = e.target.parentNode.dataset.id;
+        confirmAction(id, 'update');
       }
     }
     e.stopPropagation();
   });
 }
 
+// Confirm action and update reviews
 function confirmAction(id, type) {
+  let html;
   if (type === 'delete') {
-    let html;
     html = `
             <div class="overlay">
               <div class="main-content ${type}">
@@ -119,6 +124,29 @@ function confirmAction(id, type) {
     confirmYes.addEventListener('click', () => {
       confirmYes.textContent = 'Deleting...';
       deleteReview(id);
+    });
+    document.getElementById('confirm-no').addEventListener('click', () => {
+      document.querySelector('.overlay').remove();
+    });
+  } else if (type === 'update') {
+    html = `
+            <div class="overlay">
+              <div class="main-content ${type}">
+                <h2 class="main-content__heading ma-bt-lg">Update your review!</h2>
+                <textarea class="ma-bt-lg" id="review_text" placeholder="Your review..."></textarea>
+                <div class="confirm-buttons">
+                  <a id="confirm-yes" class="btn btn--green btn--small">Update</a>
+                  <a id="confirm-no" class="btn btn--red btn--small">Cancel</a>
+                </div>
+              </div>
+            </div>
+    `;
+    document.querySelector('body').insertAdjacentHTML('afterbegin', html);
+    const confirmYes = document.getElementById('confirm-yes');
+    confirmYes.addEventListener('click', () => {
+      confirmYes.textContent = 'Updating...';
+      const review = document.getElementById('review_text').value;
+      updateReview(id, review);
     });
     document.getElementById('confirm-no').addEventListener('click', () => {
       document.querySelector('.overlay').remove();
--- public/js/updateReview.js
+++ public/js/updateReview.js
@@ -3,6 +3,27 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+export const updateReview = async (reviewId, review) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: `/api/v1/review/${reviewId}`,
+      data: {
+        review,
+      },
+    });
+
+    if (res.data.status === 'success') {
+      showAlert(res.data.status, 'Review updated!');
+      window.setTimeout(() => {
+        location.assign('/my-reviews');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
+
 export const deleteReview = async (reviewId) => {
   try {
     const res = await axios({
